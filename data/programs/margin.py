f851f34e6f72d936f74485df0216e880bfbc4d01
arihant parsoya <parsoyaarihant@gmail.com>
2017-03-14 21:05:52 +0530

    Created margin.html

---
+++ data/programs/margin.py
@@ -0,0 +1,22 @@
+import json
+import csv
+import random
+
+data = json.load(open('margins 2017.json', 'r'))
+candidates_data = [data[i]['candidate_json'] for i in range(0, len(data))]
+final_array = []
+for i in range(0, len(candidates_data)):
+    d = {}
+    d['const_code'] = i + 1
+    d['constituency'] = data[i]['constituency']
+    #sorted_list = sorted(candidates_data[i], key=lambda k: k['votes'], reverse=True)
+    d['party'] = candidates_data[i][0]['party']
+    first_max = int(candidates_data[i][0]['votes'])
+    second_max = int(candidates_data[i][1]['votes'])
+    d['None of the Above'] = candidates_data[i][len(candidates_data[i]) - 1]['votes']
+
+    d['margin'] = first_max - second_max
+    final_array.append(d)
+
+with open('dump.txt', 'w') as outfile:
+    json.dump(final_array, outfile)
