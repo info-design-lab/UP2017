6f6dd53f733ee99d614b984f512e49b5b75a8ee9
arihant parsoya <parsoyaarihant@gmail.com>
2017-03-15 13:26:02 +0530

    Added actual data in margin.html

--- data/programs/margin.py
+++ data/programs/margin.py
@@ -10,11 +10,13 @@ for i in range(0, len(candidates_data)):
     d['const_code'] = i + 1
     d['constituency'] = data[i]['constituency']
     #sorted_list = sorted(candidates_data[i], key=lambda k: k['votes'], reverse=True)
-    d['party'] = candidates_data[i][0]['party']
+    d['win_party'] = candidates_data[i][0]['party']
+    d['win_votes'] = int(candidates_data[i][0]['votes'])
+    d['second_party'] = candidates_data[i][1]['party']
+    d['second_votes'] = int(candidates_data[i][1]['votes'])
     first_max = int(candidates_data[i][0]['votes'])
     second_max = int(candidates_data[i][1]['votes'])
     d['None of the Above'] = candidates_data[i][len(candidates_data[i]) - 1]['votes']
-
     d['margin'] = first_max - second_max
     final_array.append(d)
 
