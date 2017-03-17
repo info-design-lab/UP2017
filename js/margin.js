8ce5efb5bd698619e9999b845fff6cc92ea700ad
arihant parsoya <parsoyaarihant@gmail.com>
2017-03-17 11:17:07 +0530

    typos

--- js/margin.js
+++ js/margin.js
@@ -222,5 +222,5 @@ function makeMyMap(error, data_2002, data_2007, data_2012, data_2017) {
         highlightCard(document.getElementById('yr'+(2002 + 5*i)+ 'id' + selected_const_code));
       }
     });
-    $(".js-example-basic-single").val("261").change();
+    $(".js-example-basic-single").val("87").change();
 }
